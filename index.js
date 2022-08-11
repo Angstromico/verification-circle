cdb00e981445e3c77f75d5d51c4dfed3c79bcbd2
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2022-08-11 13:23:00 -0500

    Four Circles

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const slide = document.querySelector('.slide')
 const slide1 = document.querySelector('.slide1')
 const slide2 = document.querySelector('.slide2')
 const slide3 = document.querySelector('.slide3')
 const slide4 = document.querySelector('.slide4')
 const slide5 = document.querySelector('.slide5')
 const slide6 = document.querySelector('.slide6')
+const slide7 = document.querySelector('.slide7')
+const slide8 = document.querySelector('.slide8')
+const slide9 = document.querySelector('.slide9')
 const number = document.getElementById('number')
+const number1 = document.getElementById('number1')
 const number2 = document.getElementById('number2')
+const number3 = document.getElementById('number3')
 let counter = 0
 let counter2 = 0
 const choseSlide = (slide, number) => {
@@ -21,9 +27,13 @@ const choseSlide = (slide, number) => {
     }
   })
 }
-choseSlide(slide1, number)
-choseSlide(slide2, number)
+choseSlide(slide, number)
+choseSlide(slide1, number1)
+choseSlide(slide2, number1)
 choseSlide(slide3, number2)
 choseSlide(slide4, number2)
 choseSlide(slide5, number2)
 choseSlide(slide6, number2)
+choseSlide(slide7, number2)
+choseSlide(slide8, number2)
+choseSlide(slide9, number2)
